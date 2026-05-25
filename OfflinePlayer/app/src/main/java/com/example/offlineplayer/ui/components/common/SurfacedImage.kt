aa3f59d62b7a1ad270de3d823e1b50c8b4e15271
GabrielBall05 <gabrielb4556@gmail.com>
2026-05-25 17:35:25 -0400

    AsyncImage instead of Subcompose, fixed KeepScreenOn
    AsyncImage is not used instead of SubcomposeAsyncImage for better performance. Helper function used so that the fallback icon can still use the MaterialTheme tint (without tinting the actual working image).
    KeepScreenOn now checks for a ContextWrapper to ensure that the correct Context is being used to keep the screen on.

--- OfflinePlayer/app/src/main/java/com/example/offlineplayer/ui/components/common/SurfacedImage.kt
+++ OfflinePlayer/app/src/main/java/com/example/offlineplayer/ui/components/common/SurfacedImage.kt
@@ -10,13 +10,22 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
 import coil3.compose.SubcomposeAsyncImage
+import coil3.compose.SubcomposeAsyncImageScope
 
 @Composable
 fun SurfacedImage(
@@ -26,25 +35,81 @@ fun SurfacedImage(
     fallbackIcon: ImageVector = Icons.Default.MusicNote,
     fallbackIconTint: Color = MaterialTheme.colorScheme.surfaceTint,
     surfaceColor: Color = MaterialTheme.colorScheme.surfaceVariant,
-    sizeInDp: Dp = 40.dp
+    sizeInDp: Dp = 40.dp,
+    radius: Dp = sizeInDp / 10f
 ) {
-    Log.d("OfflineAudioSuite", "uri: $model")
+    //Common painter for error + loading image, customized by tint (defaulted to surfaceTint from MaterialTheme)
+    val fallbackPainter = rememberTintedVectorPainter(
+        image = fallbackIcon,
+        tint = fallbackIconTint
+    )
+
+    //Surface for the image to sit atop of
     Surface(
         modifier = modifier.size(sizeInDp),
-        shape = RoundedCornerShape((sizeInDp / 10.dp).dp),
+        shape = RoundedCornerShape(radius),
         color = surfaceColor
     ) {
-        SubcomposeAsyncImage(
+        //Coil is used for loading the image
+        AsyncImage(
             model = model,
             contentDescription = contentDescription,
-            contentScale = ContentScale.Crop, //TODO: Change ContentScale to something else if it crops badly
+            contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize(),
-            error = {
-                Icon(imageVector = fallbackIcon, contentDescription = "Default $contentDescription", tint = fallbackIconTint)
-            },
-            loading = {
-                Icon(imageVector = fallbackIcon, contentDescription = "Default $contentDescription", tint = fallbackIconTint)
-            }
+            placeholder = fallbackPainter,
+            error = fallbackPainter
         )
     }
 }
+
+@Composable
+fun rememberTintedVectorPainter(image: ImageVector, tint: Color): Painter {
+    val painter = rememberVectorPainter(image = image)
+    return remember(painter, tint) {
+        object : Painter() {
+            override val intrinsicSize: Size = painter.intrinsicSize
+            override fun DrawScope.onDraw() {
+                with(painter) {
+                    //Draw fallback icon with the given tint (from MaterialTheme)
+                    //This will not tint the real image (model) if it exists
+                    draw(size, colorFilter = ColorFilter.tint(tint))
+                }
+            }
+        }
+    }
+}
+
+//@Composable
+//fun SurfacedImage(
+//    model: String?,
+//    contentDescription: String,
+//    modifier: Modifier = Modifier,
+//    fallbackIcon: ImageVector = Icons.Default.MusicNote,
+//    fallbackIconTint: Color = MaterialTheme.colorScheme.surfaceTint,
+//    surfaceColor: Color = MaterialTheme.colorScheme.surfaceVariant,
+//    sizeInDp: Dp = 40.dp,
+//    radius: Dp = sizeInDp / 10f
+//) {
+//    //Common block for error + loading image
+//    val fallbackIconBlock: @Composable SubcomposeAsyncImageScope.(AsyncImagePainter.State) -> Unit = {
+//        Icon(imageVector = fallbackIcon, contentDescription = null, tint = fallbackIconTint)
+//    }
+//
+//    //Surface for the image to sit atop of
+//    Surface(
+//        modifier = modifier.size(sizeInDp),
+//        shape = RoundedCornerShape(radius),
+//        color = surfaceColor
+//    ) {
+//        //Actual image - Subcompose must be used so that error + loading image can have the custom tint
+//        SubcomposeAsyncImage(
+//            model = model,
+//            contentDescription = contentDescription,
+//            contentScale = ContentScale.Crop,
+//            modifier = Modifier.fillMaxSize(),
+//            error = fallbackIconBlock,
+//            loading = fallbackIconBlock
+//        )
+//    }
+//}
+
--- OfflinePlayer/app/src/main/java/com/example/offlineplayer/util/KeepScreenOn.kt
+++ OfflinePlayer/app/src/main/java/com/example/offlineplayer/util/KeepScreenOn.kt
@@ -1,16 +1,25 @@
 package com.example.offlineplayer.util
 
 import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import android.view.WindowManager
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.platform.LocalContext
 
+//Find Activity even if it's wrapped
+private fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
+
 @Composable
 fun KeepScreenOn() {
     val context = LocalContext.current
     DisposableEffect(Unit) {
-        val window = (context as? Activity)?.window
+        val window = context.findActivity()?.window
         window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         onDispose {
